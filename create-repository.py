fe02bc0d1092a7c88bfcad0bfc8a209f83bd074b
anhoang <anhoang@wi.mit.edu>
2023-01-11 05:43:11 -0500

    initial integration of nbdev

--- create-repository.py
+++ create-repository.py
@@ -42,27 +42,45 @@ kedro_yml_template = \
 
 makefile_template = \
 """
-project_name := test-package-project
+project_name :={project_name}
+project_python_package :={python_package_name}
+
 generate-files:
 	poetry run python create-repository.py
 
-move-kedro:
+instal-packages:
+	poetry shell
+	poetry add --dev kedro kedro-viz jupyter jupyterlab ruff slipcover toml pyyaml lineapy dvc wandb
+
+setup-kedro:
 	mv pyproject.toml pyproject_.toml
 	kedro new -c kedro-answers.yml
-	for file in $(project_name)/conf $(project_name)/data $(project_name)/docs $(project_name)/logs $(project_name)/notebooks $(project_name)/src $(project_name)/.gitignore;do mv $$file .;done
+	for file in $(project_name)/conf $(project_name)/data $(project_name)/docs $(project_name)/logs $(project_name)/notebooks $(project_name)/src;do mv $$file .;done 
+	mv $(project_name)/README.md ./README_kedro.md
+	rm -rf $(project_name)
 	mv pyproject_.toml pyproject.toml
+
+setup-post-kedro:
+	cd notebooks; mkdir package exploratory analyses generate_figures && touch package/.gitkeep exploratory/.gitkeep analyses/.gitkeep generate_figures/.gitkeep ;cd ..
+ 
+setup-nbdev:
+	nbdev_new --repo "{project_name}" --branch master --user "{author_username}" --author "{author_fullname}" --author_email "{author_email}" --description "{project_description}" --nbs_path notebooks/package --lib_path src/{python_package_name}
+
+setup-post-nbdev:
+	mv ._gitignore .gitignore
 
 clean:
 	rm -rf $(project_name)
 	rm -rf pyproject.toml
-	for file in conf data docs logs notebooks src .gitignore;do rm -rf $$file;done
+	for file in Makefile kedro-answers.yml conf data docs logs notebooks src .gitignore README_kedro.md README.md README_nbdev.md _proc .github nbs {python_package_name} LICENSE MANIFEST.in settings.ini setup.py;do rm -rf $$file;done
 
 
-reset-project: clean generate-files move-kedro
+reset-project: clean generate-files setup-kedro setup-post-kedro setup-nbdev setup-post-nbdev
+reset-project-with-install: clean generate-files instal-packages setup-kedro setup-post-kedro setup-nbdev setup-post-nbdev
  """
 
 key_val_dict = {}
-with open("copier-answers.yaml") as file:
+with open("change-my-values.yaml") as file:
     for line in file.readlines():
         if len(line.split(":")) == 2:
             key_val = [ele.strip() for ele in line.strip().split(":")]
@@ -80,4 +98,3 @@ with open("kedro-answers.yml", "w") as file:
     
 with open("Makefile", "w") as file:
     file.write(makefile_template.format(**key_val_dict))
-            
